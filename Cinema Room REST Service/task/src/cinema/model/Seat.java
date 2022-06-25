c8815b0c0ad003069c348ab3709a1bcd78496369
aili-s <95240870+aili-s@users.noreply.github.com>
2022-06-25 13:50:30 +0300

    Update Seat.java

--- Cinema Room REST Service/task/src/cinema/model/Seat.java
+++ Cinema Room REST Service/task/src/cinema/model/Seat.java
@@ -20,4 +20,11 @@ public class Seat {
     public void setRow(int row) {
         this.row = row;
     }
+    
+        public int getColumn() {
+        return column;
+    }
+    public void setColumn(int column) {
+        this.column = column;
+    }
 }
